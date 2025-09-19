86c19f8024bf16795cebd59d2357f6ce08e0fcdb
kgopotsomaifo <110554055+kgopotsomaifo@users.noreply.github.com>
2025-09-19 12:00:03 +0200

    Update seederTwo.js

--- server/seederTwo.js
+++ server/seederTwo.js
@@ -4,7 +4,7 @@ const Division = require("./models/division");
 
 // connection URL
 const mongoURI =
-  "mongodb+srv://maifokgopotso:ISQn0RjPqeSeed6A@clustercooltech.5tdx6.mongodb.net/cooltech?retryWrites=true&w=majority";
+  "mongodb+srv:@clustercooltech.5tdx6.mongodb.net/cooltech?retryWrites=true&w=majority";
 
 // Connect to the MongoDB database
 const connect = async () => {
@@ -69,3 +69,4 @@ OU.insertMany(sampleOUData)
     mongoose.disconnect();
     console.log("Disconnected from MongoDB");
   });
+
